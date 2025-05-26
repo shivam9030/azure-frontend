2417bce9b7471c65933247e6fbb9e3773fd1574d
shivam9030 <rajputshivam903@gmail.com>
2025-05-26 23:32:12 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import FoodDeliveryHome from './components/homepage'
+
+function App() {
+
+
+  return (
+    <>
+   <FoodDeliveryHome></FoodDeliveryHome>
+    </>
+  )
+}
+
+export default App
